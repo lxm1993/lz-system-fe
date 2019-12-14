48a5644df62f73b4575c2664549eee3fa920558a
简单 <1690326071@qq.com>
2019-12-14 10:49:49 +0800

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
-// const CompressionWebpackPlugin = require('compression-webpack-plugin')
-// const productionGzipExtensions = ['js', 'css']
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+const productionGzipExtensions = ['js', 'css']
 const isProduction = process.env.NODE_ENV === 'production'
 
 let pages = {}
@@ -41,16 +41,16 @@ module.exports = {
     },
     configureWebpack: config => {
         if (isProduction) {
-            // config.plugins.push(new CompressionWebpackPlugin({
-            //     algorithm: 'gzip',
-            //     test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
-            //     threshold: 10240,
-            //     minRatio: 0.8
-            // }))
-            // config.externals = {
-            //     'vue': 'Vue',
-            //     'vue-router': 'VueRouter',
-            // }
+            config.plugins.push(new CompressionWebpackPlugin({
+                algorithm: 'gzip',
+                test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
+                threshold: 10240,
+                minRatio: 0.8
+            }))
+            config.externals = {
+                'vue': 'Vue',
+                'vue-router': 'VueRouter',
+            }
         }
     },
 }
