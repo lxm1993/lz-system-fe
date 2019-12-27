f70697bcf77e28a916890112c97049e17097d32d
简单 <1690326071@qq.com>
2019-12-27 14:04:27 +0800

    csp

--- server/app/middlewares/helmet.js
+++ server/app/middlewares/helmet.js
@@ -4,8 +4,8 @@ module.exports.cspConfig = {
     directives: {
         // 默认的资源白名单
         defaultSrc: ["'self'"],
-        // 允许的脚本资源：本站点、cdn.bootcss.com、hm.baidu.com、inline资源（常见的style属性,onclick,inline js和inline css等等）
-        scriptSrc: ["'self'", 'cdn.bootcss.com'],
+        // 允许的脚本资源：本站点资源
+        scriptSrc: ["'self'"],
         // 允许的样式文件资源
         styleSrc: ["'self'"],
         // 允许的图片文件资源
@@ -20,8 +20,4 @@ module.exports.cspConfig = {
     },
     // 设为true后上述的规则不起作用，只会打印出信息
     reportOnly: false,
-    //如果设置true, 将会添加已经被抛弃的兼容头部 X-WebKit-CSP, and X-Content-Security-Policy
-    setAllHeaders: false,
-    disableAndroid: false,
-    browserSniff: true
 }
