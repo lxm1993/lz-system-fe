3cf77ff5899d7d178ac96ae3ca15a1637f755f53
简单 <1690326071@qq.com>
2019-12-27 14:01:01 +0800

    csp

--- server/app/middlewares/helmet.js
+++ server/app/middlewares/helmet.js
@@ -1,13 +1,17 @@
+//用 'unsafe-inline' 和 'unsafe-eval' 都是不安全的，它们会使您的网站有跨站脚本攻击风险。
+// 'self' 代表和文档同源，包括相同的 URL 协议和端口号
 module.exports.cspConfig = {
     directives: {
         // 默认的资源白名单
         defaultSrc: ["'self'"],
         // 允许的脚本资源：本站点、cdn.bootcss.com、hm.baidu.com、inline资源（常见的style属性,onclick,inline js和inline css等等）
-        scriptSrc: ["'self'", 'cdn.bootcss.com', 'hm.baidu.com', "'unsafe-inline'"],
+        scriptSrc: ["'self'", 'cdn.bootcss.com'],
         // 允许的样式文件资源
-        styleSrc: ["'self'", "'unsafe-inline'", 'hm.baidu.com'],
+        styleSrc: ["'self'"],
         // 允许的图片文件资源
-        imgSrc: ["'self'", 'data:', 'hm.baidu.com'],
+        imgSrc: ["'self'", 'data:'],
+        // child-src 指定定义了 web workers 以及嵌套的浏览上下文（如 <frame> 和 <iframe> ）的源
+        childSrc: ["'self'"],
         // 不太清楚
         sandbox: ['allow-forms', 'allow-scripts', 'allow-same-origin', 'allow-top-navigation', 'allow-popups'],
         // 违反上述规则后发送错误报告到下面路由
